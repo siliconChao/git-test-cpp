77bbc71bbd151c91303bc3db7f260f78034dff05
konglc <Mrchao_2022@163.com>
2023-11-14 13:27:59 +0800

    add function get_dates

--- common_def.h
+++ common_def.h
@@ -30,3 +30,82 @@ const int32 month_days[2][13] =
         {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}  // 闰年每月天数
 
 };
+
+/**
+ * @name:
+ * @msg:
+ * @param {int32} days 距离1970年的天数
+ * @return {*}
+ */
+static int32 getDate(int32 days)
+{
+    int32 year = 1970;
+    if (days > 14610)
+    {
+        // 2010年距离1970年 14610天
+        days -= 14610;
+        year = 2010;
+    }
+    else if (days > 10957)
+    {
+        // 2000年距离 1970年 10957天
+        days -= 10957;
+        year = 2000;
+    }
+
+    // 年份分摊天数 够一年天数 则年份 +1 天数 - 一年天数
+    while (year <= 2100)
+    {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) // 闰年
+        {
+            // 剩余天数超过 366 天
+            if (days > 366)
+            {
+                days -= 366;
+                year++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        else
+        {
+            if (days > 365)
+            {
+                days -= 365;
+                year++;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+    // 因为传进来的天数少一天 因此加 1
+    // days += 1;
+
+    int32 mon = 1;
+
+    int flag = 0; // 平年标记
+    if (year % 4 == 0 && year % 400 != 0 || year % 400 == 0)
+    {
+        flag = 1; // 闰年标记
+    }
+
+    // 计算月份数
+    while (mon <= 12)
+    {
+        if (days > month_days[flag][mon])
+        {
+            days -= month_days[flag][mon];
+            ++mon;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    return year * 10000 + mon * 100 + days;
+}
