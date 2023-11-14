86529761fcc128224f427176a37a850759962ca8
konglc <Mrchao_2022@163.com>
2023-11-14 13:43:11 +0800

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*
+ * @Author       : konglc
+ * @Date         : 2023-11-05 10:09:16
+ * @LastEditors  : Please set LastEditors
+ * @LastEditTime : 2023-11-14 12:41:40
+ * @Description  : version 1.1
+ */
+#include "test.h"
+int main()
+{
+    int mon = get_months(202310,202311);
+    cout << "mon : " << mon << endl;
+    return 0;
+}
