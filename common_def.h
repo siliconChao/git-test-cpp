bb2836efbe7942336818e44353257b831083566e
konglc <Mrchao_2022@163.com>
2023-11-14 13:16:16 +0800

    common_def.h file inclue modify

--- common_def.h
+++ common_def.h
@@ -1,16 +1,32 @@
 /*
  * @Author       : konglc
- * @Date         : 2023-10-23 13:44:40
+ * @Date         : 2023-10-29 14:26:18
  * @LastEditors  : Please set LastEditors
- * @LastEditTime : 2023-10-28 12:12:29
+ * @LastEditTime : 2023-11-14 12:40:29
  * @Description  : version 1.1
  */
+using namespace std;
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <stdio.h>
+#include <set>
+#include <stdlib.h>
+#include <algorithm>
+#include <iostream>
+#include <ctype.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <string.h>
 #ifndef __COMMON_DEF_H__
 #define __COMMON_DEF_H__
-    #include <iostream>
-    #include <string>
-    using namespace std;
-    typedef int int32;
-    typedef long long int64;
-#endif
 
+typedef int int32;
+typedef long long int64;
+#define FUNCTION_BGEIN cout << __FILE__ << " : " << __FUNCTION__ << " : " << __LINE__ << endl;
+const int32 month_days[2][13] =
+    {
+        {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}, // 平年每月天数
+        {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}  // 闰年每月天数
+
+};
