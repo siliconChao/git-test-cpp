83103844df27be334fa195101c8a876116223e03
konglc <Mrchao_2022@163.com>
2023-11-14 12:58:45 +0800

    C++ commom function

---
+++ common_def.h
@@ -0,0 +1,16 @@
+/*
+ * @Author       : konglc
+ * @Date         : 2023-10-23 13:44:40
+ * @LastEditors  : Please set LastEditors
+ * @LastEditTime : 2023-10-28 12:12:29
+ * @Description  : version 1.1
+ */
+#ifndef __COMMON_DEF_H__
+#define __COMMON_DEF_H__
+    #include <iostream>
+    #include <string>
+    using namespace std;
+    typedef int int32;
+    typedef long long int64;
+#endif
+
